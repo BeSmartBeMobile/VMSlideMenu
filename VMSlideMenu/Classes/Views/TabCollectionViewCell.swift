8a07bb0ae49230dd27988263dc0b8477eba9937e
Jorge Lucena <jmlucena@vectoritcgroup.com>
2017-05-19 09:34:27 +0200

    Added views

---
+++ VMSlideMenu/Classes/Views/TabCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TabCollectionViewCell.swift
+//  Pods
+//
+//  Created by Jorge Miguel Lucena Pino on 19/05/2017.
+//
+//
+
+import UIKit
+
+class TabCollectionViewCell: UICollectionViewCell {
+    
+}
