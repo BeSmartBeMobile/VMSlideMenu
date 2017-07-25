5e3a5f53e2a4ed898c83f80d91e76f71ba140a09
phonrubia <phonrubia@vectoritcgroup.com>
2017-07-25 08:49:27 +0200

    Changed options names

--- Example/VMSlideMenu/AppDelegate.swift
+++ Example/VMSlideMenu/AppDelegate.swift
@@ -18,22 +18,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let tabs = [
             MenuTab(icon: #imageLiteral(resourceName: "tab1"), selectionColor: UIColor.orange, options: [
-                MenuOption(withName: "Opcion 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 1.1 pulsada") })
+                MenuOption(withName: "Option 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 1.1 pulsada") })
                 ]),
             MenuTab(icon: #imageLiteral(resourceName: "tab2"), selectionColor: UIColor.white, options: [
-                MenuOption(withName: "Opcion 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 2.1 pulsada") }),
-                MenuOption(withName: "Opcion 2", image: #imageLiteral(resourceName: "option2"), action: { print("opción 2.2 pulsada") })
+                MenuOption(withName: "Option 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 2.1 pulsada") }),
+                MenuOption(withName: "Option 2", image: #imageLiteral(resourceName: "option2"), action: { print("opción 2.2 pulsada") })
                 ]),
             MenuTab(icon: #imageLiteral(resourceName: "tab3"), selectionColor: UIColor.red, options: [
-                MenuOption(withName: "Imputar horas", image: #imageLiteral(resourceName: "option1"), action: { print("opción 3.1 pulsada") }),
-                MenuOption(withName: "Reportar gastos", image: #imageLiteral(resourceName: "option2"), action: { print("opción 3.2 pulsada") }),
-                MenuOption(withName: "Mi perfil", image: #imageLiteral(resourceName: "option3"), action: { print("opción 3.3 pulsada") })
+                MenuOption(withName: "Option 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 3.1 pulsada") }),
+                MenuOption(withName: "Option 2", image: #imageLiteral(resourceName: "option2"), action: { print("opción 3.2 pulsada") }),
+                MenuOption(withName: "Option 3", image: #imageLiteral(resourceName: "option3"), action: { print("opción 3.3 pulsada") })
                 ]),
             MenuTab(icon: #imageLiteral(resourceName: "tab4"), selectionColor: UIColor.green, options: [
-                MenuOption(withName: "Opcion 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 4.1 pulsada") }),
-                MenuOption(withName: "Opcion 2", image: #imageLiteral(resourceName: "option2"), action: { print("opción 4.2 pulsada") }),
-                MenuOption(withName: "Opcion 3", image: #imageLiteral(resourceName: "option3"), action: { print("opción 4.3 pulsada") }),
-                MenuOption(withName: "Opcion 4", image: #imageLiteral(resourceName: "option4"), action: { print("opción 4.4 pulsada") })
+                MenuOption(withName: "Option 1", image: #imageLiteral(resourceName: "option1"), action: { print("opción 4.1 pulsada") }),
+                MenuOption(withName: "Option 2", image: #imageLiteral(resourceName: "option2"), action: { print("opción 4.2 pulsada") }),
+                MenuOption(withName: "Option 3", image: #imageLiteral(resourceName: "option3"), action: { print("opción 4.3 pulsada") }),
+                MenuOption(withName: "Option 4", image: #imageLiteral(resourceName: "option4"), action: { print("opción 4.4 pulsada") })
                 ])
         ]
         
